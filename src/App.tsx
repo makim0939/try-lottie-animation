09c238efee12f7f7ec74b8779ac8d7b3473b55d4
makim0939 <makim0939@gmail.com>
2025-09-15 22:34:45 +0900

    プロジェクトを初期化

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { DotLottieReact } from "@lottiefiles/dotlottie-react"
+
+function App() {
+
+  return (
+    <>
+     <DotLottieReact
+      src="./firework.lottie"
+      loop
+      autoplay
+      className=" w-64 h-64"
+    />
+    </>
+  )
+}
+
+export default App
